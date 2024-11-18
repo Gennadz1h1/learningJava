437a5181cf2efbb3af8ee699032fc8e470be571d
Henadz Zhukau <henadz.zhukau@leverx.com>
2024-11-18 13:07:34 +0300

    all changes

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import java.util.List;
 
 public class MainPage extends LoginPage{
-
+//
     public List<String> takeTextFromAllNavigationTabs(By locator) {
         return takeTextFromAllElements(locator);
     }

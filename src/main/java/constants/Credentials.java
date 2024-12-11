f34911b5c33272b87bdbff82588cc3ccef1d261a
Gennadz1h1 <100427082+Gennadz1h1@users.noreply.github.com>
2024-12-11 12:24:42 +0300

    Delete src directory

--- src/main/java/constants/Credentials.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class Credentials {
-    public String userEmail = "henadzzhukau@gmail.com";
-    public String password = "123Fg546";
-}
--- src/main/java/constants/PartnersPageConstants.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class PartnersPageConstants {
-    public String completelyTransparentBackgroundColor = "rgba(0, 0, 0, 0)";
-    public String darkGrayBackgroundColor = "rgba(72, 72, 74, 1)";
-    public String pageName = "Partners and solutions";
-    public String backgroundColor = "background-color";
-}
